8ca9cd3e5119c118ac4b31e80d288d41fa6d7d4e
Gavin Niederman <gavinniederman@gmail.com>
2022-12-28 01:19:59 -0800

    fix example bin

--- src/bin/printputs.rs
+++ src/bin/printputs.rs
@@ -1,18 +1,26 @@
-use std::{sync::mpsc, thread};
 use saunter::listener::Listener;
+use std::{sync::mpsc, thread};
 
 fn main() {
     let (t, x) = mpsc::channel::<winit::event::Event<'_, ()>>();
-    let mut loop_ = saunter::tickloop::Loop::new(Box::new(PrinterListener {}), 60, x);
+    let mut loop_ = saunter::tickloop::Loop::new(Box::new(PrinterListener {}), 2, x);
     thread::spawn(move || loop_.start());
 
     let event_loop = winit::event_loop::EventLoop::new();
-    event_loop.run( move |event, _, control_flow| {
+    let _window = winit::window::WindowBuilder::new()
+        .build(&event_loop)
+        .unwrap();
+    event_loop.run(move |event, _, control_flow| {
         control_flow.set_poll();
-        if let winit::event::Event::WindowEvent { event: winit::event::WindowEvent::CloseRequested, .. } = event {
-            
+        if let winit::event::Event::WindowEvent {
+            event: winit::event::WindowEvent::CloseRequested,
+            ..
+        } = event {
+            control_flow.set_exit();
         }
-        //TODO: fix this t.send(event.to_owned()).unwrap();
+
+        t.send(event.to_static().unwrap_or(winit::event::Event::NewEvents(winit::event::StartCause::Poll)))
+            .unwrap_or_else(|err| eprintln!("{:?}", err));
     })
 }
 
@@ -20,7 +28,7 @@ struct PrinterListener {}
 impl Listener for PrinterListener {
     fn tick(&mut self, _dt: f32, events: &mut Vec<winit::event::Event<'_, ()>>) {
         for event in events {
-            println!("{:?}", event);
+            println!("Tick {:?}", event);
         }
     }
 }

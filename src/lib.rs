e2f27cf7ccfc1df9884f4778a664fba45d314311
Gavin Niederman <gavinniederman@gmail.com>
2022-12-28 14:11:55 -0800

    fix reversed lerp

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
 pub mod listener;
 pub(crate) mod math;
 pub mod tickloop;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn lerp() {
+        assert_eq!(math::lerp(0.0, 100.0, 0.75).unwrap(), 75.0);
+
+        let vals = vec![(0.0, 1.0), (0.5, 1.0), (0.0, 2.0)];
+        assert_eq!(math::lerp_vec(vals, 0.5).unwrap(), vec![0.5, 0.75, 1.0]);
+    }
+}
--- src/math.rs
+++ src/math.rs
@@ -1,11 +1,13 @@
+#[inline]
 pub fn lerp(a: f32, b: f32, t: f32) -> Result<f32, &'static str> {
     if t < 0.0 || t > 1.0 {
         return Err("t must be between 0 and 1");
     }
 
-    Ok(a * t + b * (1.0 - t))
+    Ok(a * (1.0 - t) + b * t)
 }
 
+#[inline]
 pub fn lerp_vec(list: Vec<(f32, f32)>, t: f32) -> Result<Vec<f32>, &'static str> {
     let mut result = Vec::new();
 

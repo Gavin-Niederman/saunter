3d294234cc6d15ba8a7778eb9164cacb82a3bfd6
Gavin-Niederman <gavinniederman@gmail.com>
2023-01-16 22:40:48 -0800

    update readdme

--- README.md
+++ README.md
@@ -2,12 +2,6 @@
 
 A library for tick based game engines.
 
-## What is a tick based game engine?
-
-A tick based game engine runs all of it's calculations at fixed intervals. This is in contrast to most modern game engines which run their calculations every frame. Tick based game engines are used because it allows for calculations to be run on a different thread than the rendering thread. allowing for the game to render at a consistent speed even if the rendering thread is slow, and for the rendering thread to run quickly even if the calculations are slow.
-
-When multiple frames happen per tick interpolation is used to make the game appear to run smoothly. This is done by storing the tick data for the previous tick, and the most recent tick and interpolating between the two to get what the renderer should show.
-
 ## How to use Saunter
 
 ### Reading this in it's entirety is recommended
@@ -39,7 +33,7 @@ impl Listener for ExampleListener {
 }
 ```
 
-You may have noticed the type Tick being used a lot here. Ticks store a snapshot of the state of the listener. The tick function returns the tick that was just processed. Similarly to making a listener we implement the Tick trait on our tick type.
+You may have noticed the type Tick being used a lot here. Ticks store a snapshot of the state of the listener. The tick function returns the tick that was just processed. Similarly to making a listener we implement the Tick trait on our tick type. In a game engine a tick would only ever be used for rendering. no game logic should happen in the render thread. Because we only need to send things used for rendering in the tick you should try your best to limit what gets sent in a tick for optimization.
 
 ```rust
 struct ExampleTick {
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,11 @@
-//! The core of saunter is the [`Loop`](crate::tickloop::Loop). The [`Loop`](crate::tickloop::Loop) calls the [`tick()`](crate::listener::Listener::tick) function on the provided listener. Your listener should store the state of your game engine or the current scene in your engine. To create a listener implement [`Listener`](crate::listener::Listener).
+//! # Saunter
+//!
+//! A library for tick based game engines.
+//!
+//! ## How to use Saunter
+//!
+//! The core of saunter is the loop. The loop calls the tick function on the provided listener. 
+//! Your listener should store the state of your game engine or the current scene in your engine.
 //!
 //! ```rust
 //! struct ExampleListener {
@@ -6,7 +13,7 @@
 //! }
 //! impl Listener for ExampleListener {
 //!     // The type of the tick that tick function returns
-//!     type TickType = ExampleTick;
+//!     type TickType = ExampleTick; 
 //!     // The type of event that is passed to tick in the Event::Other(EventType) variant
 //!     type EventType = ExampleEvent;
 //!
@@ -25,7 +32,11 @@
 //! }
 //! ```
 //!
-//! You may have noticed the type [`Tick`](crate::tick::Tick) being used a lot here. Ticks store a snapshot of the state of the listener. The [`tick()`](crate::listener::Listener::tick) function returns the tick that was just processed. Similarly to making a listener we implement the [`Tick`](crate::tick::Tick) trait on our tick type.
+//! You may have noticed the type Tick being used a lot here.
+//! Ticks store a snapshot of the state of the listener. The tick function returns the tick that was just processed.
+//! In a game engine a tick would only ever be used for rendering. 
+//! no game logic should happen in the render thread. 
+//! Because we only need to send things used for rendering in the tick you should try your best to limit what gets sent in a tick for optimization.
 //!
 //! ```rust
 //! struct ExampleTick {
@@ -46,28 +57,32 @@
 //!
 //!     fn get_time(&self) -> &Instant {
 //!         &self.time
-//!     }
+//!     } 
 //! }
 //! ```
 //!
-//! Now we can create the loop! The easiest way to create a loop is using [`Loop::init()`](crate::tickloop::Loop::init)
+//! Now we can create the loop! The easiest way to create a loop is using `Loop::init()`
 //!
 //! ```rust
 //! let (mut tick_loop, event_sender, ticks) = Loop::init(
 //!     // The listener that the loop will call tick() on
 //!     Box::new(listener),
 //!     // The state of the engine or scene when the program is started. AKA the first tick
-//!     first_tick,
+//!     first_tick, 
 //!     // The number of ticks to occur per second (TPS), does not have to be an integer
 //!     tps,
 //! );
 //! ```
 //!
-//! Now that you have a loop all that is left is to send it events. To do this you can use the event_sender that was returned by [`Loop::init()`](crate::tickloop::Loop::init). The event sender is a `Sender<Event<EventType>>` where EventType is the type of event you specified in your listener. **EventType is wrapped in a [`Event`](crate::event::Event)**. This is to guarentee that you can send a close event to the loop. To send an event you can use the [`send()`](std::sync::mpsc::Sender::send()) method on the event sender.
+//! Now that you have a loop all that is left is to send it events. To do this you can use the event_sender that was returned by `Loop::init()`. The event sender is a `Sender<Event<EventType>>` where EventType is the type of event you specified in your listener. **EventType is wrapped in a ``saunter::event::Event``**. This is to guarentee that you can send a close event to the loop. To send an event you can use the `send()` method on the event sender.
 //!
 //! ### Wait? What is a Ticks?
 //!
-//! The [`Ticks`](crate::tick::Ticks) type is used to store the most recent and last tick. It has a lerp funtion that returns a new tick interpolated by the amount specified between the two ticks for use in rendering, or whatever you choose. The ticks returned by [`Loop::init()`](crate::tickloop::Loop::init) is not actually a ticks, it is a ``Arc<RwLock<Ticks<...>>>``, This is because it is constantly being updated by the tickloop. For this reason, **only ever have a read lock on ticks**. To help with optimization, it is best practise to immediatly drop the read lock when you are done with it.
+//! The ticks type is used to store the most recent and last tick. 
+//! It has a lerp funtion that returns a new tick interpolated by the amount specified between the two ticks for use in rendering, or whatever you choose.
+//! The ticks returned by ``Loop::init()`` is not actually a ticks, it is a ``Arc<RwLock<Ticks<...>>>``, 
+//! This is because it is constantly being updated by the tickloop. For this reason, **only ever have a read lock on ticks**.
+//! To help with optimization, it is best practise to immediatly drop the read lock when you are done with it.
 
 pub mod error;
 pub mod event;
